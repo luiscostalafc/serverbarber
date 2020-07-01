8ea006a833cc00cccbcc55b7dc8bef7c11c6359c
luiscostalafc <luiscostalafc@gmail.com>
2020-07-01 16:51:54 -0300

    Add phones for seeds

--- src/database/seeds/20200605214735-users.js
+++ src/database/seeds/20200605214735-users.js
@@ -7,6 +7,7 @@ module.exports = {
 			[
 				{
 					name: 'Luis Costa',
+					phone: '(41) 99999-9999',
 					email: 'luiscostalafc@gmail.com',
 					password_hash: bcrypt.hashSync('123456', 8),
 					provider: false,
@@ -15,6 +16,7 @@ module.exports = {
 				},
 				{
 					name: 'Provider 1',
+					phone: '(41) 98999-9999',
 					email: 'provider1@gmail.com',
 					password_hash: bcrypt.hashSync('secret', 8),
 					provider: true,
@@ -23,6 +25,7 @@ module.exports = {
 				},
 				{
 					name: 'Provider 2',
+					phone: '(41) 97999-9999',
 					email: 'provider2@gmail.com',
 					password_hash: bcrypt.hashSync('secret', 8),
 					provider: true,
@@ -31,6 +34,7 @@ module.exports = {
 				},
 				{
 					name: 'Provider 3',
+					phone: '(41) 96999-9999',
 					email: 'provider3@gmail.com',
 					password_hash: bcrypt.hashSync('secret', 8),
 					provider: true,
