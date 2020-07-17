a2520766dfdb8c540d8f02e9c65ff2671978516b
luiscostalafc <luiscostalafc@gmail.com>
2020-07-17 02:27:34 -0300

    path

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -9,7 +9,7 @@ class File extends Model {
 				url: {
 					type: Sequelize.VIRTUAL,
 					get() {
-						return `${this.path}`;
+						return `http://jackhair-barber.com.br/files/${this.path}`;
 					},
 				},
 			},
