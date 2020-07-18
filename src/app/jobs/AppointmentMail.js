c67839a3b5b13464206e98c6670a4eaa21c48c6d
EmersonBraun <emersonfbraun@gmail.com>
2020-07-18 12:58:00 -0300

    fix: ensure appointment mail be sended

--- src/app/jobs/AppointmentMail.js
+++ src/app/jobs/AppointmentMail.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { format, parseISO } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import coloredLog from '../../lib/ColoredLog';
@@ -9,24 +10,56 @@ class AppointmentMail {
 	}
 
 	async handle({ data }) {
-		try {
-			const { appointment } = data;
-
-			await Mail.sendMain({
-				to: `${appointment.user.name}  <${appointment.user.email}>`,
-				subject: 'Novo agendamento',
-				template: 'appointment',
-				context: {
-					provider: appointment.provider.name,
-					user: appointment.user.name,
-					services: appointment.items,
-					date: format(
+		const { appointment } = data;
+		const to =
+			appointment && appointment.user && appointment.user.name
+				? appointment.user.name
+				: 'TO';
+		console.error(coloredLog(`📨 AppointmentMail handle to: ${to}`));
+		const email =
+			appointment && appointment.user && appointment.user.email
+				? appointment.user.email
+				: 'Mail';
+		console.error(coloredLog(`📨 AppointmentMail handle email: ${email}`));
+		const provider =
+			appointment && appointment.provider && appointment.provider.name
+				? appointment.provider.name
+				: 'PROVIDER';
+		console.error(
+			coloredLog(`📨 AppointmentMail handle provider: ${provider}`)
+		);
+		const user =
+			appointment && appointment.user && appointment.user.name
+				? appointment.user.name
+				: 'user';
+		console.error(coloredLog(`📨 AppointmentMail handle user: ${user}`));
+		const services =
+			appointment && appointment.items ? appointment.items : 'services';
+		console.error(
+			coloredLog(`📨 AppointmentMail handle services: ${services}`)
+		);
+		const date =
+			appointment && appointment.date
+				? format(
 						parseISO(appointment.date),
 						"'dia' dd 'de' MMMM', às' H:mm'h'",
 						{
 							locale: pt,
 						}
-					),
+				  )
+				: 'DATE';
+		console.error(coloredLog(`📨 AppointmentMail handle date: ${date}`));
+
+		try {
+			await Mail.sendMain({
+				to: `${to}  <${email}>`,
+				subject: 'Novo agendamento',
+				template: 'appointment',
+				context: {
+					provider,
+					user,
+					services,
+					date,
 				},
 			});
 		} catch (error) {
